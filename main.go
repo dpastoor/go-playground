907db0a556b21f1d28b42584fa1e7a981f279058
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-04 08:45:28 -0500

    showing how os exit works in a goroutine, such that it exits before all others finish

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
 	"sync"
 	"time"
 )
@@ -12,32 +11,18 @@ import "os"
 var wg sync.WaitGroup
 
 func main() {
-	basewd, _ := os.Getwd()
 	wg.Add(3)
 	for i := 1; i < 4; i++ {
-		go changeDirInGoRoutine(basewd, i)
+		go osExitInGoRoutine(i)
 	}
 	wg.Wait()
 }
 
-func changeDirInGoRoutine(baseDir string, routineNum int) {
+func osExitInGoRoutine(routineNum int) {
 	defer wg.Done()
-	wd, _ := os.Getwd()
-	fmt.Printf("routine %v: starting in dir %s\n", routineNum, wd)
-	os.Chdir(filepath.Join(baseDir, fmt.Sprintf("%s%v", "r", routineNum)))
-	wd, _ = os.Getwd()
-	fmt.Printf("routine %v, going to sleep in dir: %s\n", routineNum, wd)
+	fmt.Printf("routine %v: starting \n", routineNum)
 	time.Sleep(1000 * time.Millisecond)
-	wd, _ = os.Getwd()
-	fmt.Printf("routine %v, woke up in dir %s\n", routineNum, wd)
+	fmt.Printf("routine %v: woke up and ready to do more stuff \n", routineNum)
+	fmt.Printf("routine %v: about to exit", routineNum)
+	os.Exit(1)
 }
-
-// routine 2: starting in dir C:\golang\src\github.com\dpastoor\playground
-// routine 3: starting in dir C:\golang\src\github.com\dpastoor\playground
-// routine 1: starting in dir C:\golang\src\github.com\dpastoor\playground
-// routine 2, going to sleep in dir: C:\golang\src\github.com\dpastoor\playground\r2
-// routine 3, going to sleep in dir: C:\golang\src\github.com\dpastoor\playground\r3
-// routine 1, going to sleep in dir: C:\golang\src\github.com\dpastoor\playground\r1
-// routine 3, woke up in dir C:\golang\src\github.com\dpastoor\playground\r1
-// routine 2, woke up in dir C:\golang\src\github.com\dpastoor\playground\r1
-// routine 1, woke up in dir C:\golang\src\github.com\dpastoor\playground\r1
